f75582d2a7d073ed05e9ea65338a03f7bdd115e4
mirco <mirco.mardones.quilapi@gmail.com>
2016-09-15 15:17:01 -0300

    aqui esta la tarea completa

---
+++ los_numeros.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main (){
+	float c1=55,c2=40,c3=50, NP;
+	NP = (c1 * 0.25)+(c2 * 0.35)+(c3 * 0.4);
+	printf("%i",&NP);
+	
+	return 0 ;
+}
+//Diseñe un programa que calcule la nota de presentación de un ramo si se conoce las notas de los certámenes del alumno. La nota de presentación se calcula así: 
+//NP= 25% C1 + 35 % C2 + 40 % C3
